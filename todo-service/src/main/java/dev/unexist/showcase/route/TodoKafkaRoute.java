71ccd2cf067a4875d1006ef123a0b3fa1097343c
Christoph Kappel <christoph@unexist.dev>
2023-06-26 17:53:49 +0200

    Updated to Jakarta

--- todo-service/src/main/java/dev/unexist/showcase/route/TodoKafkaRoute.java
+++ todo-service/src/main/java/dev/unexist/showcase/route/TodoKafkaRoute.java
@@ -17,7 +17,7 @@ import org.apache.camel.model.rest.RestBindingMode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.context.ApplicationScoped;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
